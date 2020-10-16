8645eed5c75b2cfd37b3c53a4f1c9dd30241bc81
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-16 22:42:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |char, index|
-    num = 1
-    puts "#{num}.*#{char}"
-    num += 1
+    puts "#{index}. #{char}"
   end
 end
